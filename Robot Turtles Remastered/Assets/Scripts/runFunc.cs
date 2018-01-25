fed031709cd3f6dcefb2b83d2374c0f153c32777
caps03 <caps01@udayton.edu>
2018-01-25 10:42:27 -0500

    update turtle sprite, gameboard, movement

--- Robot Turtles Remastered/Assets/Scripts/runFunc.cs
+++ Robot Turtles Remastered/Assets/Scripts/runFunc.cs
@@ -6,16 +6,26 @@ public class runFunc : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        runFunct();
+        GameObject Square_1 = GameObject.Find("Square 57");
+        GameObject curPlayer = (GameObject)Instantiate(Resources.Load("turtle_0"));
+        curPlayer.transform.SetParent(Square_1.transform);// = hand.transform;
+        //runFunct();
 
     }
-
+    /*
+1   2   3   4   5   6   7   8
+9   10  11  12  13  14  15  16
+17  18  19  20  21  22  23  24
+25  26  27  28  29  30  31  32 
+33  34  35  36  37  38  39  40
+41  42  43  44  45  46  47  48
+49  50  51  52  53  54  55  56 
+57  58  59  60  61  62  63  64
+     */
     public void runFunct()
     {
-        GameObject Square_1 = GameObject.Find("Square_1");
-        GameObject curPlayer = (GameObject)Instantiate(Resources.Load("Turtle0"));
-        curPlayer.transform.SetParent(Square_1.transform);// = hand.transform;
-        //GameObject curPlayer = GameObject.Find("Turtle0(Clone)");
+        
+        GameObject curPlayer = GameObject.Find("Turtle0(Clone)");
         var parent = transform.parent.name;
         Debug.Log(parent);
 
@@ -26,8 +36,9 @@ public class runFunc : MonoBehaviour {
             string tag = child.tag;
             if (tag == "Forward")
             {
-                if (curRot.z == 0)
+                if (curRot.z == -90)
                 {
+                    if (parent != "Square 1")
                     curPlayer.transform.Translate(Vector3.up * 2);//(Vector3.up * Time.deltaTime);
                 }
                 else if (curRot.z == 90)

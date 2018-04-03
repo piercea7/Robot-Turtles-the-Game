ec8e33614dd53a98b79f4f655bbf6b383874dfd3
caps03 <caps01@udayton.edu>
2018-04-03 11:12:36 -0400

    changed assets

--- Robot Turtles Remastered/Assets/Networking/Launcher.cs
+++ Robot Turtles Remastered/Assets/Networking/Launcher.cs
@@ -148,7 +148,7 @@ public class Launcher : PunBehaviour
         Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
 
         // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.automaticallySyncScene to sync our instance scene.
-        if (PhotonNetwork.room.PlayerCount == 1)
+        if (PhotonNetwork.room.PlayerCount == 4)
         {
             Debug.Log("We load the 'Room for 1' ");
 
